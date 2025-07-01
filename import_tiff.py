a11a2b85bf7d94a33e13746d6e0d3eeabd58af83
FlyBozon <m.flybozon@gmail.com>
2025-07-01 19:17:52 +0200

    better options to read tiff image

--- import_tiff.py
+++ import_tiff.py
@@ -1,7 +1,23 @@
 import rasterio
 import matplotlib.pyplot as plt
-src = rasterio.open('./datasets/UAV_VisLoc_dataset/01/satellite01.tif')
+
+import cv2
+import tifffile
+import matplotlib.pyplot as plt
+
+image_path = './datasets/UAV_VisLoc_dataset/01/satellite01.tif'
+
+src = rasterio.open(image_path)
 array = src.read(1)
 #print(array.shape)
-plt.imshow(array, cmap='ocean')
+plt.imshow(array)
+plt.show()
+
+img_tiff = tifffile.imread(image_path)
+img_cv2=cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+
+plt.imshow(cv2.cvtColor(img_cv2, cv2.COLOR_BGR2RGB)) #imread read in BGR instead of RGB, so image needs to be converted to RGB before showing
+plt.show()
+
+plt.imshow(img_tiff)
 plt.show()
